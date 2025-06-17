3b84c88e4093ae63e847f613b28af5c8aeb5ce9a
ErnestoPalma122 <112669496+ErnestoPalma122@users.noreply.github.com>
2025-06-16 22:59:08 -0600

    Avance2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Home } from './pages/home/home';
 import { About } from './pages/about/about';
 import { Contact } from './pages/contact/contact';
 import { Productos } from './pages/products/products';
+import { Login } from './pages/login/login';
 
 export const routes: Routes = [
     {path:'', component: Home},
     {path:'nosotros', component: About},
     {path:'productos',component: Productos},
     {path:'contacto',component:Contact},
+    {path:'login',component:Login},
     {path:'**',redirectTo:''}
 ];
